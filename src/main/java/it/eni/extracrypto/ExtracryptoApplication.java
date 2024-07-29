42493f9dd4ea31bbf807a4e51adbe25894e3cb0b
nino <Antox93@googlemail.com>
2024-07-29 19:24:50 +0200

    first commit

---
+++ src/main/java/it/eni/extracrypto/ExtracryptoApplication.java
@@ -0,0 +1,13 @@
+package it.eni.extracrypto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtracryptoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtracryptoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/eni/extracrypto/ExtracryptoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.eni.extracrypto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtracryptoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
