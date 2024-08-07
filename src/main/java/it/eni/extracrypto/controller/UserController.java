937d4240136fda4e91b98eb7017f0c58a4dadd06
nino <Antox93@googlemail.com>
2024-08-07 20:04:25 +0200

    Api Login

--- src/main/java/it/eni/extracrypto/controller/UserController.java
+++ src/main/java/it/eni/extracrypto/controller/UserController.java
@@ -1,22 +1,38 @@
 package it.eni.extracrypto.controller;
 
 import it.eni.extracrypto.model.dto.CreateUserDto;
+import it.eni.extracrypto.model.entity.User;
 import it.eni.extracrypto.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping ("user")
 public class UserController {
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
 
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @PostMapping
     public void createUser (@RequestBody CreateUserDto request){
         userService.createUser(request);
+    }
+
+    @GetMapping
+    public ResponseEntity<User> login(@RequestHeader("Authorization") String auth){
+        User user= userService.login(auth);
+
+        if (user!= null){
+            return ResponseEntity.ok(user);
+        }else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+
     }
 }
--- src/main/java/it/eni/extracrypto/service/UserService.java
+++ src/main/java/it/eni/extracrypto/service/UserService.java
@@ -8,6 +8,7 @@ import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 
@@ -29,10 +30,32 @@ public class UserService {
 
     }
 
+    public User login(String auth){
+        byte[] decodedAuth = Base64.getDecoder().decode(auth.replaceAll("Basic ", ""));
+        String username = new String(decodedAuth, StandardCharsets.UTF_8).split(":")[0];
+        String password =  new String(decodedAuth, StandardCharsets.UTF_8).split(":")[1];
+
+        User user = userRepository.findByUsername(username);
+
+        if(user != null){
+            String salt = user.getPassword().split(":")[0];
+            String hashedPasswordUser = user.getPassword().split(":")[1];
+
+            byte[] hashedPasswordBytes = DigestUtils.sha256(salt + password);
+            String hashedPassword = Base64.getEncoder().encodeToString(hashedPasswordBytes);
+            if (hashedPassword.equals(hashedPasswordUser)){
+                return user;
+            }
+        }
+        return null;
+    }
+
+
     private String generatePassword(String password){
         String salt = Utils.generateSalt();
         byte[]  hashedPassword = DigestUtils.sha256(salt+password);
        return salt+ ":"+ Base64.getEncoder().encodeToString(hashedPassword);
 
     }
+
 }
